0b242f67afb62cfe27e69af5252dd4dbfe486fa5
ZhykharievAnatolii <nekijt87@gmail.com>
2022-11-17 20:09:07 +0200

    upd
    upd for js add delete button

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 "use strict"
+// const list=document.querySelector('ul');
+// const input=document.querySelector('input');
+// const button=document.querySelector('button');
+// button.onclick=()=>{
+//     const value=input.value;
+//     renderItem(value);
+//     input.value='';
+// }
+// function renderItem(text) {
+//     const item=document.createElement('li');
+//     item.innerText=text;
+//     list.append(item);
+// }
 const list=document.querySelector('ul');
 const input=document.querySelector('input');
 const button=document.querySelector('button');
+
 button.onclick=()=>{
     const value=input.value;
     renderItem(value);
@@ -9,6 +23,12 @@ button.onclick=()=>{
 }
 function renderItem(text) {
     const item=document.createElement('li');
+    const deleteButton=document.createElement('button');
+    deleteButton.innerText='X';
+    deleteButton.onclick=()=>{
+        item.remove();
+    };
     item.innerText=text;
+    item.append(deleteButton);
     list.append(item);
 }
